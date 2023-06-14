eb5928756a2b71d21c48b737f1358029e7a29e2b
Eric Mittelhammer <eric@erics-mbp.mynetworksettings.com>
2023-06-14 16:36:30 -0400

    switch to a framework that can be auto-instrumented with OTel

--- api/src/main/java/Main.java
+++ api/src/main/java/Main.java
@@ -1,26 +1,50 @@
-import com.google.common.base.Charsets;
-import com.google.common.base.Supplier;
-import com.google.common.base.Suppliers;
-import com.sun.net.httpserver.HttpHandler;
-import com.sun.net.httpserver.HttpServer;
+import com.linecorp.armeria.common.HttpRequest;
+import com.linecorp.armeria.common.HttpResponse;
+import com.linecorp.armeria.common.HttpStatus;
+import com.linecorp.armeria.common.MediaType;
+
+import com.linecorp.armeria.server.AbstractHttpService;
+import com.linecorp.armeria.server.Server;
+import com.linecorp.armeria.server.ServerBuilder;
+import com.linecorp.armeria.server.ServiceRequestContext;
 
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.sql.*;
+import java.util.Map;
+import java.util.AbstractMap;
 import java.util.NoSuchElementException;
+import java.util.concurrent.CompletableFuture;
 
 public class Main {
+
+    private static Map<String, String> tables = Map.ofEntries(
+        new AbstractMap.SimpleEntry<String, String>("noun", "nouns"),
+        new AbstractMap.SimpleEntry<String, String>("verb", "verbs"),
+        new AbstractMap.SimpleEntry<String, String>("adjective", "adjectives")
+    );
     public static void main(String[] args) throws Exception {
         Class.forName("org.postgresql.Driver");
 
-        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
-        server.createContext("/noun", handler(() -> randomWord("nouns")));
-        server.createContext("/verb", handler(() -> randomWord("verbs")));
-        server.createContext("/adjective", handler(() -> randomWord("adjectives")));
-        server.start();
+        ServerBuilder sb = Server.builder();
+        // Configure an HTTP port.
+        sb.http(8080);
+        sb.service("/{wordtype}", new AbstractHttpService() {
+            @Override
+            protected HttpResponse doGet(ServiceRequestContext ctx, HttpRequest req) {
+                String wordType = ctx.pathParam("wordtype");
+                String word = randomWord(wordType);
+                return HttpResponse.of(HttpStatus.OK, MediaType.JSON_UTF_8,
+                               "{\"word\":\"%s\"}", word);
+            }
+        });
+        Server server = sb.build();
+        CompletableFuture<Void> future = server.start();
+        future.join();
     }
 
-    private static String randomWord(String table) {
+    private static String randomWord(String wordType) {
+        String table = tables.get(wordType);
         try (Connection connection = DriverManager.getConnection("jdbc:postgresql://db:5432/postgres", "postgres", "")) {
             try (Statement statement = connection.createStatement()) {
                 try (ResultSet set = statement.executeQuery("SELECT word FROM " + table + " ORDER BY random() LIMIT 1")) {
@@ -36,22 +60,4 @@ public class Main {
         throw new NoSuchElementException(table);
     }
 
-    private static HttpHandler handler(Supplier<String> word) {
-        return t -> {
-            String response = "{\"word\":\"" + word.get() + "\"}";
-            byte[] bytes = response.getBytes(Charsets.UTF_8);
-
-            System.out.println(response);
-            
-            t.getResponseHeaders().add("content-type", "application/json; charset=utf-8");
-            t.getResponseHeaders().add("cache-control", "private, no-cache, no-store, must-revalidate, max-age=0");
-            t.getResponseHeaders().add("pragma", "no-cache");
-
-            t.sendResponseHeaders(200, bytes.length);
-
-            try (OutputStream os = t.getResponseBody()) {
-                os.write(bytes);
-            }
-        };
-    }
 }
